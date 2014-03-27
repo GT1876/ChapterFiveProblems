5402d1681ba5b761f448d028c5d51ad8bf4fff56
GT1876 <garrysontime@gmail.com>
2014-03-27 10:07:28 -0400

    abcdefg

---
+++ ChapterFiveProblems/IntegerList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegerList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Number = new int[5];
+            for(int i = 0; i < 5; i++)
+            { 
+                Number[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
