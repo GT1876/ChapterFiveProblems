ef7c0bfd3d620e558f579f401acba72f180ec770
Jazz-Aka-Amazing <jazsingh12825@gmail.com>
2014-03-31 10:08:49 -0400

    Really Big Turds

--- ChapterFiveProblems/IntegerList/Program.cs
+++ ChapterFiveProblems/IntegerList/Program.cs
@@ -9,10 +9,20 @@ namespace IntegerList
     {
         static void Main(string[] args)
         {
-            int[] Number = new int[5];
-            for(int i = 0; i < 5; i++)
-            { 
-                Number[i] = Convert.ToInt32(Console.ReadLine());
+           int[] num = new int[] {1, 5, 3, 2, 4};
+
+            Array.Sort(num);
+            for (int i = 0; i < 5; i++)
+            {
+                //
+                Console.WriteLine(num[i]);
+            }
+            Array.Reverse(num);
+            for (int i = 0; i < 5; i++)
+            {
+                //
+                Console.WriteLine(num[i]);
+            }
         }
     }
 }
