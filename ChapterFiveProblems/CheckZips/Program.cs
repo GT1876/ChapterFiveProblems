15ee51b9a3a7828a43db4d7832f6ef6b3f561399
Jazz-Aka-Amazing <jazsingh12825@gmail.com>
2014-04-02 10:08:21 -0400

    Gargauntuan Turds

--- ChapterFiveProblems/CheckZips/Program.cs
+++ ChapterFiveProblems/CheckZips/Program.cs
@@ -9,7 +9,31 @@ namespace CheckZips
     {
         static void Main(string[] args)
         {
-            
+            int[] zip = new int[10]{18042, 36548, 98624, 13597, 24584, 36489, 36897, 21549, 12654, 15034};
+
+
+            Console.Write("Enter one of the limited zipcodes: ");
+            String input = Console.ReadLine();
+            int n = Convert.ToInt32(input);
+            int index = -1;
+
+
+
+            for (int x = 0; x < zip.Length; x++)
+            {
+                if (n == zip[x])
+                {
+                    index++;
+                    Console.WriteLine("Your package will be recived in 5-7 buisness days.");
+                }
+
+            }
+            if (index == -1)
+            {
+                Console.WriteLine("Sorry we cant ship it there");
+            }
+
+
         }
     }
 }
